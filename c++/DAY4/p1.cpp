87fe4619973b9238bb7005b1ea2c9df474c3a104
GovindSharma06 <govindsharmazero6@gmail.com>
2026-07-20 09:15:09 +0530

    day4 practice

---
+++ c++/DAY4/p1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    std :: cout << "Enter a number: ";
+    
+    int num;
+
+    std :: cin >> num;
+
+    std :: cout <<  "two times this number is: " << (2 * num) ;
+
+    return 0;
+}
